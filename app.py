af6f4fa8999ae5560582cb25375d4f25a2799f30
kiyunamakiki <sunflower.ma33@gmail.com>
2022-06-08 07:06:18 +0900

    first3

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 app.secret_key = 'secret_key'
 
 # --------ここから書く---------
-@app.route("")
+@app.route("/list")
+def list():
+        py_user_id = session["user_id"]
+        conn = sqlite3.connect("flasktest.db")
+        c = conn.cursor()
+        c.execute("select id, task from tasks WHERE user_id = ?",(py_user_id,))
+        task_list = []
+        for row in c.fetchall():
+            task_list.append({"id":row[0],"task":row[1]})
+        c.close()
+        print(task_list)
+        return render_template("list.html",task_list = task_list )
+    else:
+        return redirect("/login")
 
 
 
@@ -16,3 +29,4 @@ app.secret_key = 'secret_key'
 
 if __name__ == "__main__":
     app.run(debug=True)
+    if "user_id" in session:
