6e43c804b5908fb4ed9b70463a003272e1880f1a
kiyunamakiki <sunflower.ma33@gmail.com>
2022-06-09 00:36:13 +0900

    firs43

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from  flask import Flask
+from  flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
 # --------ここから書く---------
-@app.route("/list")
-def list():
-        py_user_id = session["user_id"]
-        conn = sqlite3.connect("flasktest.db")
-        c = conn.cursor()
-        c.execute("select id, task from tasks WHERE user_id = ?",(py_user_id,))
-        task_list = []
-        for row in c.fetchall():
-            task_list.append({"id":row[0],"task":row[1]})
-        c.close()
-        print(task_list)
-        return render_template("list.html",task_list = task_list )
-    else:
-        return redirect("/login")
+@app.route('/')
+def index():
+    return '<h1>Hello Team Cookie</h1>'
+
+@app.route('/top')
+def template():
+    py_name = "cookie"
+    return render_template("index.html")
+
+# @app.route("/list")
+# def list():
+#         py_user_id = session["user_id"]
+#         conn = sqlite3.connect("flasktest.db")
+#         c = conn.cursor()
+#         c.execute("select id, task from tasks WHERE user_id = ?",(py_user_id,))
+#         task_list = []
+#         for row in c.fetchall():
+#             task_list.append({"id":row[0],"task":row[1]})
+#         c.close()
+#         print(task_list)
+#         return render_template("list.html",task_list = task_list )
+#     else:
+#         return redirect("/login")
 
 
 
@@ -29,4 +38,3 @@ def list():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    if "user_id" in session:
