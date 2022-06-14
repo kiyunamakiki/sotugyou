b1d5137b8cb8f2890c2aea2dd978b56cc75d2cb1
kiyunamakiki <sunflower.ma33@gmail.com>
2022-06-14 23:24:53 +0900

    kiyuna_TOP(/)へ変更

--- app.py
+++ app.py
@@ -12,23 +12,21 @@ app = Flask(__name__)
 app.secret_key = 'sunabaco'
 
 # --------ここから書く---------
+# top画面
 @app.route('/')
-def index():
-    return '<h1>Hello Team Cookie</h1>'
+def top():
+    return render_template("index.html")
 
+# まだやってない　できとらん6/14_22:43時点
 @app.route('/greet/<name>')
 def greet(name):
     return name + 'さん、こんにちは！' 
 
+# まだやってない　できとらん6/14_22:43時点
 @app.route('/template')
 def template():
     py_name = "SUNABACO"
     return render_template("index.html",name = py_name)
-
-# test list
-@app.route("/test")
-def test():
-    return render_template("mainlist.html")
 
 # login画面用＿残す
 @app.route("/login")
@@ -78,8 +76,7 @@ def add_post():
 # 新規ユーザー登録
 @app.route("/regist")
 def regist():
-    return render_template("index.html")
-    # regist.html
+    return render_template("regist.html")
 
 @app.route("/regist",methods = ["POST"])
 def regist_post():
@@ -91,10 +88,9 @@ def regist_post():
         c.execute("INSERT INTO members VALUES(null,?,?)",(py_name,py_password))
         conn.commit()
         conn.close()
-        return "登録しました" 
+        return redirect("/list")
 
-
-# タスクリスト表示
+# タスクリスト
 @app.route("/list")
 def list():
     if "user_id" in session:
@@ -114,7 +110,7 @@ def list():
     else:
         return redirect("/login")
 
-# 声かけランダム
+# どんなタスクにする？声かけランダム
 @app.route('/word')
 def word():
     word_list = ["１分でできることもいいね！","ずーっと気になってることやってみる？","１日１つのタスクもいいね！","カップラーメンできる間にやっちゃう？","深呼吸する時間意識してみる？","朝起きてコップ１杯の白湯いいらしいよ！","自分を褒めまくるのもいいね"]
@@ -138,6 +134,12 @@ def edit(id):
             return render_template("edit.html",task_id = id,task = task)
     else:
         return redirect("/login")
+
+
+# # edit
+# @app.route('/edit')
+# def edit():
+#     return render_template("edit.html")
 
 @app.route("/edit",methods = ["POST"])
 def edit_post():
@@ -166,111 +168,31 @@ def del_get(id):
     conn.close()
     return redirect("/list")
 
-
-
+# ログアウト
 @app.route("/logout")
 def logout():
     session.pop("user_id",None)
     return redirect("/login")
 
-
-# -------いったん隠しー－－－－－－－－－－－
-# @app.route('/top')
-# def template():
-#     py_name = "cookie"
-#     return render_template("index.html")
-
-
-# # タスク入力
-# @app.route("/add",methods = ["GET"])
-# def add():
-#     return render_template("add.html")
-
-# @app.route("/add",methods = ["POST"])
-# def add_post():
-#     py_task = request.form.get("task")
-
-#     print(py_task)
-#     # py_user_id = session["user_id"]
-#     # py_task = request.form.get("task")
-
-#     # conn = sqlite3.connect('flasktest.db')
-#     # c = conn.cursor()
-#     # c.execute("INSERT INTO tasks (taks,user_id) VALUES(?,?)",(py_task,user_id))
-#     # conn.commit()
-#     # conn.close()
-#     # return redirect("/list")
-
-# # edit
-# @app.route('/edit')
-# def edit():
-#     return render_template("edit.html")
-
-# # list一覧
-# @app.route('/list')
-# def list():
-#     return render_template("list.html")
-
-# # login
-# @app.route('/login')
-# def login():
-#     return render_template("login.html")
-
-# # regist
-# @app.route('/regist')
-# def regist():
-#     return render_template("regist.html")
-
-# # 以下未使用
-# # @app.route('/')
-# # def ():
-# #     return render_template(".html")
-
-# # @app.route('/')
-# # def ():
-# #     return render_template(".html")
-
-# # @app.route('/')
-# # def ():
-# #     return render_template(".html")
-
-# # @app.route('/')
-# # def ():
-# #     return render_template(".html")
-
-# # @app.route('/')
-# # def ():
-# #     return render_template(".html")
-
-# # @app.route("/list")
-# # def list():
-# #         py_user_id = session["user_id"]
-# #         conn = sqlite3.connect("flasktest.db")
-# #         c = conn.cursor()
-# #         c.execute("select id, task from tasks WHERE user_id = ?",(py_user_id,))
-# #         task_list = []
-# #         for row in c.fetchall():
-# #             task_list.append({"id":row[0],"task":row[1]})
-# #         c.close()
-# #         print(task_list)
-# #         return render_template("list.html",task_list = task_list )
-# #     else:
-# #         return redirect("/login")
-
-
+# ４０３
 @app.errorhandler(403)
 def mistake403(code):
     return 'There is a mistake in your url!'
 
-
+# ４０４
 @app.errorhandler(404)
 def notfound404(code):
     return "404だよ！！見つからないよ！！！ぱわー！"
 
+# -------コピペ用ー－－－－－－－－－－－
 
+# @app.route('/')
+# def ():
+#     return render_template(".html")
 
+# -------ー－－－－－－－－－－－
 
-# ---------ここまで書く----------
+# ---------ここまで書く、以下消さない----------
 
 if __name__ == "__main__":
     app.run(debug=True)
